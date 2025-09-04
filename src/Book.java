20ca087f4dc79bff171c98ce1fc6da8e6e4cb84d
DxniChid <chidiroglou.danai06@gmail.com>
2025-09-04 16:05:43 +0200

    Started Project

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    public int id;
+    public String title;
+    public String author;
+    public int quantity;
+
+    public Book(int id, String title, String author, int quantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+    }
+}
