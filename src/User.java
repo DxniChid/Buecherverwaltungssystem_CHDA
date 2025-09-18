4e1199d30e1a504eb8cf8608e39fdf2adee8ff63
DxniChid <chidiroglou.danai06@gmail.com>
2025-09-18 15:46:17 +0200

    class User

---
+++ src/User.java
@@ -0,0 +1,20 @@
+public abstract class User {
+
+
+    protected int id;
+    protected String username;
+    protected String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+
+    public abstract void showMenu();
+
+
+}
+
