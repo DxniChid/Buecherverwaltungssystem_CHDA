7c16d8eabee3eadc99a1f1804b5cf1a0d7a465d3
DxniChid <chidiroglou.danai06@gmail.com>
2025-09-18 14:22:05 +0200

    Override, Getter/Setter

--- src/Book.java
+++ src/Book.java
@@ -10,4 +10,19 @@ public class Book {
         this.author = author;
         this.quantity = quantity;
     }
+    // Getter und Setter
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                " | Title: " + title +
+                " | Author: " + author +
+                " | Quantity: " + quantity;
+    }
 }
