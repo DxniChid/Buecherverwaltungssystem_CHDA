9a243a57e8bbaebffc6642348f57cff98a60875b
DxniChid <chidiroglou.danai06@gmail.com>
2025-10-02 08:57:05 +0200

    Public zu private

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,8 @@
 public class Book {
-    public int id;
-    public String title;
-    public String author;
-    public int quantity;
+    private int id;
+    private String title;
+    private String author;
+    private int quantity;
 
     public Book(int id, String title, String author, int quantity) {
         this.id = id;
